cf9af3a51e6246b070f0af875ccdceac5abc1647
Jawilson20 <Wilsonjakolbi57@gmail.com>
2025-04-22 01:29:06 -0500

    s

--- MongoDB/student-records-backend/server.js
+++ MongoDB/student-records-backend/server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./dbconfig');
 const studentRoutes = require('./routes/studentroutes');
+const cors = require('cors'); // Move this up
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/students', studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const cors = require('cors');
-app.use(cors());
-
