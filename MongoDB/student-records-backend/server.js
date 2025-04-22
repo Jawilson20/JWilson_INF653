97054b3ea424a53fcedb1c65f9d5b3d6f5f25b0b
Jawilson20 <Wilsonjakolbi57@gmail.com>
2025-04-22 01:04:22 -0500

    log

--- MongoDB/student-records-backend/server.js
+++ MongoDB/student-records-backend/server.js
@@ -13,3 +13,7 @@ app.use('/students', studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const cors = require('cors');
+app.use(cors());
+
